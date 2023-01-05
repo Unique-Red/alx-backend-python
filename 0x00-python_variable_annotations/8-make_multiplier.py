e213d536e8c49332d41a4745c5e99baceefc8faa
unique-red <noah13victor@gmail.com>
2023-01-05 17:03:49 +0000

    functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''functions'''
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''return float * multiplier'''
+    def new_function (n: float) -> float:
+        '''multiplies the float by mulitiplier'''
+        return float (n * multiplier)
+
+    return new_function
