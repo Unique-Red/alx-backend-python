c23d5b9818088751ea53b7159447a29ed5e60abc
unique-red <noah13victor@gmail.com>
2023-01-05 17:39:54 +0000

    first element sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''if-else statements'''
     if lst:
         return lst[0]
     else:
