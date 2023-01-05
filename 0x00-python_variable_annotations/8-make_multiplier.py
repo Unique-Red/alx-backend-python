c333775528df07c27c9259a96876535f51c5881f
unique-red <noah13victor@gmail.com>
2023-01-05 17:05:24 +0000

    functions: fix pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,8 +7,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''return float * multiplier'''
-    def new_function (n: float) -> float:
+    def new_function(n: float) -> float:
         '''multiplies the float by mulitiplier'''
-        return float (n * multiplier)
+        return float(n * multiplier)
 
     return new_function
