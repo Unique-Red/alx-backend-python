d4ab107217a1a7f47c147e809a0e9f4ea595ed4c
unique-red <noah13victor@gmail.com>
2023-01-05 16:02:31 +0000

    list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''complex types - list of floats'''
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''adds floats'''
+    return float(sum(input_list))
